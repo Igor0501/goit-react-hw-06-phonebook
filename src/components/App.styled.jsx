3f004804583abb00e900a3800b6b7f72ded1ffbb
Igor0501 <s.igor0501@gmail.com>
2023-08-05 03:36:59 +0300

    first optimize

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 100px auto;
+  padding: 40px;
+  width: 500px;
+  box-shadow: 5px 5px 10px 5px rgba(0, 0, 0, 0.75);
+`;
+
+export const Ul = styled.ul`
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
