1821be27f0d13d84dcf002269f274340870c906f
Igor0501 <s.igor0501@gmail.com>
2023-08-06 12:42:31 +0300

    add

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -4,7 +4,7 @@ export const Container = styled.div`
   margin: 100px auto;
   padding: 30px;
   width: 500px;
-  box-shadow: 5px 5px 10px 5px rgba(0, 0, 0, 0.75);
+  box-shadow: 5px 5px 10px 10px rgba(0, 0, 0, 0.75);
 `;
 
 export const Ul = styled.ul`
